3a00bc59d9e56e5a53e67e39b26c06c5f37dfca1
sriharika <sudha200215@gmail.com>
2023-04-06 22:54:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const[modal,setState] = useState(false)
+  const[data,setData]=useState({
+    name:" ",
+    email: " ",
+    feedback: " "
+  })
+  const handleChange = (e)=>{
+
+    const { name, value} = e.target
+    console.log(value)
+    setData({...data,[name]:value})
+  }
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='middle'>
+      <h1 className='h1'><center>Feedback Form</center></h1>
+      {!modal && (
+      <button onClick={() => setState((value) => !value)}>Toogle Feedback</button>
+      )}
+      <div className='feedback'>
+        {modal && (
+        <form onSubmit={handleSubmit}>
+          <button onClick={()=> setState((value) => !value)}>X</button>
+          <div>
+            <input placeholder='Enter your name' name='name' onChange={(e)=>handleChange(e)}/>
+          </div>
+          <div>
+            <input placeholder='Enter your EMail' name='email' onChange={(e)=>handleChange(e)}/>
+          </div>
+          <div>
+            <input placeholder='Enter your Feedback' name='feedback' onChange={(e)=>handleChange(e)}/>
+          </div>
+          <button onClick={handleSubmit}>Submit Feedback</button>
+        </form>
+        )}
+      </div>
+      </div>
     </div>
   );
 }
